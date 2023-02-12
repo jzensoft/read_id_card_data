0152006f2ba6b7611d6fbc5e9bb6d97db90add51
DESKTOP-ICD36D9\Weha <jzen.soft@gmail.com>
2023-02-12 18:16:15 +0700

    add upload image

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+import os.path
 
-from read_data import reader
+from flask import Flask, request, abort
+
+import read_data
+
+UPLOAD_FOLDER = 'assets/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello():
-    return reader('assets/images/id_card.jpg', 'th', 0, False)
+@app.route('/', methods=['POST'])
+def upload_image():
+    try:
+        if 'image' not in request.files:
+            return abort(400, "Please, select image")
+        image = request.files['image']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        image.save(path)
+        return read_data.reader(path, 'th', 0, False)
+    except BaseException as e:
+        abort(400, str(e))
 
 
 if __name__ == "__main__":
